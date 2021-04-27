e96a1fa44d5eeabfe15ae64062acb2ca2bb83927
Elenor <elenor@elenor.net>
2021-04-26 19:23:47 -0700

    :lipstick: refactor: updated slider hover and focus styles

--- src/components/Form/Slider/StyledSlider.ts
+++ src/components/Form/Slider/StyledSlider.ts
@@ -5,27 +5,16 @@ export interface StyledSliderProps extends SliderProps {
   trackBackground: string;
   trackFillBackground: string;
   handleBackgroundColor: string;
-  hoverHandleBoxShadow: string;
-  hoverHandleBorderColor: string;
   tickBackgroundColor: string;
 }
 
 export const StyledSlider = styled(Slider, {
   shouldForwardProp: (prop: string) =>
-    ![
-      'trackBackground',
-      'trackFillBackground',
-      'handleBackgroundColor',
-      'hoverHandleBoxShadow',
-      'hoverHandleBorderColor',
-      'tickBackgroundColor',
-    ].includes(prop),
+    !['trackBackground', 'trackFillBackground', 'handleBackgroundColor', 'tickBackgroundColor'].includes(prop),
 })<StyledSliderProps>`
   --slider-track-background-color: ${props => props.trackBackground};
   --slider-track-fill-background-color: ${props => props.trackFillBackground};
   --slider-handle-background-color: ${props => props.handleBackgroundColor};
-  --slider-hover-handle-box-shadow: 0 0 0 1px ${props => props.hoverHandleBoxShadow};
-  --slider-hover-handle-border-color: ${props => props.hoverHandleBorderColor};
   --slider-tick-background-color: ${props => props.tickBackgroundColor};
 `;
 
