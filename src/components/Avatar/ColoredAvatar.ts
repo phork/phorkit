a5e4f7ea5b259533dbb2e7c6a3af715146a61fdc
Elenor <elenor@elenor.net>
2021-05-05 09:51:06 -0700

    :sparkles: feat: added colored avatar

---
+++ src/components/Avatar/ColoredAvatar.ts
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import { Theme } from '../../types';
+import { themes, ThemeColors, ThemeColorIds } from '../../config';
+import { withTheme } from '../../context/Theme';
+import { Avatar, AvatarProps } from './Avatar';
+
+export interface ColoredAvatarProps extends Omit<AvatarProps, 'themeId'> {
+  colorId: ThemeColorIds;
+  themeId: Theme;
+}
+
+// [TODO:ts] revisit casting
+const StyledAvatar = styled(Avatar, {
+  shouldForwardProp: (prop: string): boolean => !['colorId', 'themeId', 'variant'].includes(prop),
+})<ColoredAvatarProps>`
+  --avatar-background-color: ${props => themes[props.themeId][`color-${props.colorId}` as keyof ThemeColors]};
+  --avatar-text-color: ${props => themes[props.themeId][`color-${props.colorId}-contrast` as keyof ThemeColors]};
+`;
+
+export const ColoredAvatar = React.memo<ColoredAvatarProps>(withTheme(StyledAvatar));
+ColoredAvatar.displayName = 'ColoredAvatar';
+
+StyledAvatar.defaultProps = {
+  unthemed: true,
+};
