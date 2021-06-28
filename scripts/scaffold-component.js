a92119b83be2fe76bf27722ddecc4fca3964ef6d
Elenor <elenor@elenor.net>
2021-06-28 12:53:28 -0700

    :hammer: build: add parent option to scaffold component script

--- scripts/scaffold-component.js
+++ scripts/scaffold-component.js
@@ -12,6 +12,7 @@ program
   .version(pkg.version)
   .usage('[options]')
   .addOption(new program.Option('-t, --type <type>', 'type (component or composition)').choices(['component', 'composition']).default('component'))
+  .option('-p, --parent <parent>', 'parent component (optional)')
   .requiredOption('-c, --component <component>', 'component name');
 
 program.on('--help', function(){
@@ -20,12 +21,15 @@ program.on('--help', function(){
   console.log('  $ scaffold-component --help');
   console.log('  $ scaffold-component -c Button');
   console.log('  $ scaffold-component -c Tooltip -t composition');
+  console.log('  $ scaffold-component -c ButtonGroup -p Button');
 });
 
 program.parse(process.argv);
 
-const { component, type = 'component' } = program.opts();
-const src = `./src/${type}s/${component}`;
+const { component, type = 'component', parent } = program.opts();
+const src = `./src/${type}s/${parent || component}`;
+const testSrc = `./__tests__/${type}s/${parent || component}`;
+const relativeSrc = '../'.repeat(2 + (parent ? (parent.match(/\//g) || []).length : 0));
 
 if (typeof component === 'undefined') {
   console.log(chalk.red('Missing argument -c or --component. Try --help'))
@@ -46,8 +50,8 @@ const ucfirst = input => {
 writeFileSyncRecursive(`${src}/${component}.tsx`, `
 import React from 'react';
 import { cx } from '@emotion/css';
-import { ThemeProps } from '../../types';
-import { useThemeId } from '../../hooks/useThemeId';
+import { ThemeProps } from '${relativeSrc}types';
+import { useThemeId } from '${relativeSrc}hooks/useThemeId';
 import styles from './styles/${component}.module.css';
 
 export interface ${component}Props extends ThemeProps {
@@ -72,11 +76,6 @@ export function ${component}({ children, themeId: initThemeId }: ${component}Pro
 ${component}.displayName = '${component}';
 `);
 
-// generate the component index
-writeFileSyncRecursive(`${src}/index.ts`, `
-export * from './${component}';
-`);
-
 // generate the styles boilerplate
 writeFileSyncRecursive(`${src}/styles/${component}.module.css`, `
 .${lcfirst(component)} {
@@ -96,7 +95,7 @@ import { PageTitle } from 'docs/helpers/PageTitle';
 import { ThemeWrapper } from 'docs/helpers/ThemeWrapper';
 import { ${component} } from '../index';
 
-<PageTitle title="${component}" src="${type}s/${component}" />
+<PageTitle title="${component}" src="${type}s/${parent || component}" />
 
 ## Basic ${lcfirst(component)}
 
@@ -111,8 +110,39 @@ import { ${component} } from '../index';
 <Props of={${component}} />
 `);
 
+// generate the tests boilerplate
+writeFileSyncRecursive(`${testSrc}/${component}.test.tsx`, `
+import { render } from '@testing-library/react'
+import React from 'react'
+import { ${component} } from 'lib'
+
+describe('<${component} />', () => {
+  it('should render', () => {
+    const { queryByText } = render(
+      <${component}>Hello world</${component}>,
+    )
+    expect(queryByText('Hello world')).toBeTruthy()
+  })
+})
+`)
+
+// generate or update the component index
+if (fs.existsSync(`${src}/index.ts`)) {
+  fs.appendFileSync(
+    `${src}/index.ts`,
+    `export * from './${component}';\n`
+  );
+} else {
+  writeFileSyncRecursive(
+    `${src}/index.ts`,
+    `export * from './${component}';\n`
+  );
+}
+
 // add the export to the index file
-const lines = [`export * from './${component}';`, ...fs.readFileSync(`src/${type}s/index.ts`).toString().split('\n')].sort()
-writeFileSyncRecursive(`src/${type}s/index.ts`, [...new Set(lines)].join('\n'))
+if (!parent) {
+  const lines = [`export * from './${component}';`, ...fs.readFileSync(`src/${type}s/index.ts`).toString().split('\n')].sort()
+  writeFileSyncRecursive(`src/${type}s/index.ts`, [...new Set(lines)].join('\n'))
+}
 
 console.log(chalk.green(`🚀 Generated ${src}`));
