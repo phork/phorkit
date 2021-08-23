e2fdb14e2b65d6f84746fbd25076411365c2db07
Elenor <elenor@elenor.net>
2021-08-23 14:13:32 -0700

    :sparkles: feat: added Flex stories

---
+++ src/components/Flex/stories/Flex.stories.tsx
@@ -0,0 +1,171 @@
+import { ArgsTable, Description, Primary, Stories, Subtitle, PRIMARY_STORY } from '@storybook/addon-docs';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react';
+import { themes } from 'config/themes';
+import { useThemeId } from 'context/Theme/useThemeId';
+import { Looper } from 'docs/helpers/Looper';
+import { PageTitle } from 'stories/helpers/PageTitle';
+import { Flex, FlexProps } from '../Flex';
+
+export default {
+  title: 'Utilities/Flex',
+  component: Flex,
+  argTypes: {
+    children: {
+      control: {
+        disable: true,
+      },
+    },
+
+    direction: {
+      options: ['row', 'column'],
+      control: { type: 'inline-radio' },
+      table: {
+        category: 'Primary controls',
+      },
+    },
+    alignContent: {
+      options: [undefined, 'center', 'flex-end', 'flex-start', 'space-around', 'space-between', 'stretch'],
+      control: { type: 'radio' },
+      table: {
+        category: 'Primary controls',
+      },
+    },
+    alignItems: {
+      options: [undefined, 'baseline', 'center', 'flex-end', 'flex-start'],
+      control: { type: 'radio' },
+      table: {
+        category: 'Primary controls',
+      },
+    },
+    alignSelf: {
+      options: [undefined, 'baseline', 'center', 'flex-end', 'flex-start', 'space-around', 'space-between', 'stretch'],
+      control: { type: 'radio' },
+      table: {
+        category: 'Primary controls',
+      },
+    },
+    justifyContent: {
+      options: [undefined, 'center', 'flex-end', 'flex-start', 'space-around', 'space-between', 'space-evenly'],
+      control: { type: 'radio' },
+      table: {
+        category: 'Primary controls',
+      },
+    },
+    flexible: {
+      table: {
+        category: 'Primary controls',
+      },
+    },
+    full: {
+      table: {
+        category: 'Primary controls',
+      },
+    },
+    inflexible: {
+      table: {
+        category: 'Primary controls',
+      },
+    },
+    inline: {
+      table: {
+        category: 'Primary controls',
+      },
+    },
+    max: {
+      table: {
+        category: 'Primary controls',
+      },
+    },
+    reverse: {
+      table: {
+        category: 'Primary controls',
+      },
+    },
+    wrap: {
+      table: {
+        category: 'Primary controls',
+      },
+    },
+
+    style: {
+      table: {
+        category: 'Uncommon controls',
+      },
+    },
+  },
+  parameters: {
+    controls: {
+      exclude: ['className'],
+      sort: 'requiredFirst',
+    },
+    docs: {
+      page: () => (
+        <React.Fragment>
+          <PageTitle src="components/Flex" title="Flex" />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <ArgsTable story={PRIMARY_STORY} />
+          <Stories />
+        </React.Fragment>
+      ),
+      description: {
+        component: 'A component to add flexbox styling to a container div.',
+      },
+    },
+  },
+  includeStories: ['Default'],
+} as ComponentMeta<typeof Flex>;
+
+export function FlexBox({
+  children,
+  height,
+  width,
+  ...props
+}: {
+  children: React.ReactChild;
+  height: number | string;
+  width: number | string;
+}) {
+  const themeId = useThemeId();
+  return (
+    <div
+      style={{
+        alignItems: 'center',
+        backgroundColor: themes[themeId]['color-accent-primary'],
+        border: `1px solid ${themes[themeId]['color-accent-primary-contrast']}`,
+        color: themes[themeId]['color-accent-primary-contrast'],
+        display: 'flex',
+        height,
+        justifyContent: 'center',
+        width,
+        ...props,
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export const defaultArgs = {
+  children: (
+    <Looper
+      end={5}
+      render={i => (
+        <FlexBox height={40 + i * 10} key={i} width={40 + i * 10}>
+          {i}
+        </FlexBox>
+      )}
+      start={0}
+    />
+  ),
+  direction: 'row' as FlexProps['direction'],
+};
+
+export const Template: ComponentStory<typeof Flex> = (args: FlexProps) => <Flex {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  ...defaultArgs,
+};
--- src/docs/helpers/Looper.tsx
+++ src/docs/helpers/Looper.tsx
@@ -1,19 +1,32 @@
 import React, { cloneElement, Fragment } from 'react';
 
-export interface LooperProps {
+type NumericLooperProps = {
   end: number;
-  start: number;
-  step: number;
-  render: (item: string | number, i: number) => React.ReactElement;
-  list?: Array<string | number>;
-}
+  start?: number;
+  step?: number;
+  render: (item: number) => React.ReactElement;
+};
 
-export function Looper({ end: initEnd, list, render, start = 0, step = 1 }: LooperProps) {
+type ListLooperProps = {
+  render: (item: string, i?: number) => React.ReactElement;
+  list: Array<string>;
+};
+
+// to be used as a type guard
+const isListLooper = (list: undefined | ListLooperProps['list']): list is ListLooperProps['list'] => {
+  return list !== undefined && Array.isArray(list);
+};
+
+export function Looper(props: NumericLooperProps): React.ReactElement;
+
+export function Looper(props: ListLooperProps): React.ReactElement;
+
+export function Looper({ end: initEnd, list, render, start = 0, step = 1 }: any): React.ReactElement {
   const items = [];
   const end = list ? list.length - 1 : initEnd;
 
   for (let i = start; i <= end; i += step) {
-    const element = render(list ? list[i] : i, i);
+    const element = isListLooper(list) ? render(list[i], i) : render(i);
 
     items.push(
       cloneElement(element, {
