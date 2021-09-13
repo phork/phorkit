6bd4b11e5e6d39520d9e40c2a5d79149a99b064f
Elenor <elenor@elenor.net>
2021-09-13 11:02:39 -0700

    :recycle: refactor: use custom Canvas for storybook

--- .storybook/components/DocsContainer.tsx
+++ .storybook/components/DocsContainer.tsx
@@ -1,11 +1,16 @@
-import { DocsContainer as BaseContainer } from '@storybook/addon-docs';
+import { DocsContainer as BaseContainer, DocsContainerProps as BaseContainerProps } from '@storybook/addon-docs';
 import { themes } from '@storybook/theming';
 import React from 'react';
 import { useDarkMode } from 'storybook-dark-mode';
 import { ErrorBoundary } from '../../src/components/ErrorBoundary';
 import { Toasts } from '../../src/compositions/Toast';
 
-export const DocsContainer = ({ children, context }) => {
+interface DocsContainerProps {
+  children: React.ReactNode;
+  context: BaseContainerProps['context'];
+}
+
+export const DocsContainer = ({ children, context }: DocsContainerProps) => {
   const dark = useDarkMode();
 
   return (
