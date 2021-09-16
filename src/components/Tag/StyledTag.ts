42bf138a7b4835aa91faaed10ce9728cf9b9efab
Elenor <elenor@elenor.net>
2021-09-16 14:52:45 -0700

    :lipstick: refactor: updated Button and Tag hover and active states

--- src/components/Tag/StyledTag.ts
+++ src/components/Tag/StyledTag.ts
@@ -4,14 +4,19 @@ import { Tag, TagProps } from './Tag';
 export interface StyledTagProps extends TagProps {
   primaryColor: string;
   inverseColor: string;
+  hoveredPrimaryColor?: string;
+  activePrimaryColor?: string;
 }
 
 // @ts-ignore [TODO:ts] WTF
 export const StyledTag = styled(Tag, {
-  shouldForwardProp: (prop: string) => !['primaryColor', 'inverseColor', 'themeId'].includes(prop),
+  shouldForwardProp: (prop: string) =>
+    !['primaryColor', 'inverseColor', 'hoveredPrimaryColor', 'activePrimaryColor', 'themeId'].includes(prop),
 })<StyledTagProps>`
   ${({ primaryColor }) => primaryColor && `--tag-primary-color: ${primaryColor};`}
   ${({ inverseColor }) => inverseColor && `--tag-inverse-color: ${inverseColor};`}
+  ${({ hoveredPrimaryColor }) => hoveredPrimaryColor && `--tag-hovered-primary-color: ${hoveredPrimaryColor};`}
+  ${({ activePrimaryColor }) => activePrimaryColor && `--tag-active-primary-color: ${activePrimaryColor};`}
 `;
 
 StyledTag.displayName = 'StyledTag';
