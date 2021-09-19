5781e9d6dee2d14fde2915dc91972c57414e4172
Elenor <elenor@elenor.net>
2021-09-19 13:07:45 -0700

    :sparkles: feat: added Label stories

---
+++ src/components/Form/Label/stories/Label.stories.tsx
@@ -0,0 +1,155 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react';
+import { Label, LabelProps } from '../Label';
+import LabelDocumentation from './Label.docs.mdx';
+
+export default {
+  title: 'Form/Label',
+  component: Label,
+  argTypes: {
+    children: {
+      control: { type: 'text' },
+    },
+
+    noWrap: {
+      table: {
+        category: 'Appearance controls',
+      },
+    },
+    strength: {
+      table: {
+        category: 'Appearance controls',
+      },
+    },
+    validity: {
+      options: ['success', 'warning', 'danger', undefined],
+      table: {
+        category: 'Appearance controls',
+      },
+    },
+
+    disabled: {
+      table: {
+        category: 'State controls',
+      },
+    },
+    focused: {
+      table: {
+        category: 'State controls',
+      },
+    },
+    muted: {
+      table: {
+        category: 'State controls',
+      },
+    },
+
+    as: {
+      table: {
+        category: 'Uncommon controls',
+      },
+    },
+    contrast: {
+      table: {
+        category: 'Uncommon controls',
+      },
+    },
+    style: {
+      table: {
+        category: 'Uncommon controls',
+      },
+    },
+    unthemed: {
+      table: {
+        category: 'Uncommon controls',
+      },
+    },
+  },
+  parameters: {
+    controls: {
+      exclude: ['className', 'themeId'],
+      sort: 'requiredFirst',
+    },
+    docs: {
+      page: LabelDocumentation,
+      description: {
+        component: 'A new component.',
+      },
+    },
+    layout: 'centered',
+  },
+} as ComponentMeta<typeof Label>;
+
+const Template: ComponentStory<typeof Label> = args => <Label {...args} />;
+
+const defaultArgs = {
+  children: 'Super fantastic label',
+  contrast: false,
+  disabled: false,
+  focused: false,
+  muted: false,
+  noWrap: false,
+  strength: 'standard' as LabelProps['strength'],
+  unthemed: false,
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  ...defaultArgs,
+};
+
+export const StandardStrength = Template.bind({});
+StandardStrength.storyName = 'Strength: Standard';
+StandardStrength.args = {
+  ...defaultArgs,
+  strength: 'standard',
+};
+
+export const TransitionedStrength = Template.bind({});
+TransitionedStrength.storyName = 'Strength: Transitioned';
+TransitionedStrength.args = {
+  ...defaultArgs,
+  strength: 'transitioned',
+};
+
+export const LegendStrength = Template.bind({});
+LegendStrength.storyName = 'Strength: Legend';
+LegendStrength.args = {
+  ...defaultArgs,
+  strength: 'legend',
+};
+
+export const SuccessValidity = Template.bind({});
+SuccessValidity.storyName = 'Validity: Success';
+SuccessValidity.args = {
+  ...defaultArgs,
+  validity: 'success',
+};
+
+export const WarningValidity = Template.bind({});
+WarningValidity.storyName = 'Validity: Warning';
+WarningValidity.args = {
+  ...defaultArgs,
+  validity: 'warning',
+};
+
+export const DangerValidity = Template.bind({});
+DangerValidity.storyName = 'Validity: Danger';
+DangerValidity.args = {
+  ...defaultArgs,
+  validity: 'danger',
+};
+
+export const Muted = Template.bind({});
+Muted.storyName = 'Muted';
+Muted.args = {
+  ...defaultArgs,
+  muted: true,
+};
+
+export const Disabled = Template.bind({});
+Disabled.storyName = 'Disabled';
+Disabled.args = {
+  ...defaultArgs,
+  disabled: true,
+};
