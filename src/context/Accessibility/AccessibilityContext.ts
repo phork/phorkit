e2c9a5ab41fb4f273c5ae2b99d6bf515ac34208e
Elenor <elenor@elenor.net>
2021-09-30 11:12:25 -0500

    :recycle: refactor(major): renamed focusType to eventType in Accessibility context

--- src/context/Accessibility/AccessibilityContext.ts
+++ src/context/Accessibility/AccessibilityContext.ts
@@ -1,18 +1,18 @@
 import { createContext } from 'react';
 
-export type FocusType = 'keyboard' | 'mouse' | 'touch';
+export type EventType = 'keyboard' | 'mouse' | 'touch';
 
 export interface AccessibilityContextValue {
   /** If the latest navigation event was by keyboard this is true */
   accessible?: boolean;
   /** The source of the latest navigation event */
-  focusType?: FocusType;
+  eventType?: EventType;
   /** Sets the value that's used to determine accessible flag */
-  setFocusType: (focusType: FocusType) => void;
+  setEventType: (eventType: EventType) => void;
 }
 
 export const AccessibilityContext = createContext<AccessibilityContextValue>({
   accessible: false,
-  focusType: undefined,
-  setFocusType: (/* focusType */) => {},
+  eventType: undefined,
+  setEventType: (/* eventType */) => {},
 });
--- src/context/Scale/ScaleContext.ts
+++ src/context/Scale/ScaleContext.ts
@@ -2,9 +2,7 @@ import { createContext } from 'react';
 import { IconScale } from '../../types';
 
 export interface ScaleContextValue {
-  /** The scale is a string that maps to a numeric size (eg. small, medium, large) */
   scale?: IconScale;
-  /** The exact numeric size */
   size?: number;
 }
 
