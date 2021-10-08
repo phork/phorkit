febebbf6420826ba72453ea77fdda01f24034ed6
Elenor <elenor@elenor.net>
2021-10-07 20:50:12 -0700

    :white_check_mark: test: updated IconGroup tests to check size

--- __tests__/compositions/IconGroup/IconGroup.test.tsx
+++ __tests__/compositions/IconGroup/IconGroup.test.tsx
@@ -1,9 +1,10 @@
+import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 import React from 'react';
 import { IconGroup, TimesIcon } from 'lib';
 
 describe('<IconGroup />', () => {
-  it('should render a basic icon group', () => {
+  it('should render a basic icon group by scale', () => {
     const { container } = render(
       <IconGroup scale="large">
         <TimesIcon />
@@ -15,5 +16,21 @@ describe('<IconGroup />', () => {
       </IconGroup>,
     );
     expect(container.querySelectorAll('svg').length).toBe(6);
+    expect(container.querySelector('svg')).toHaveAttribute('width', '20px');
+  });
+
+  it('should render a basic icon group by size', () => {
+    const { container } = render(
+      <IconGroup size={12}>
+        <TimesIcon />
+        <TimesIcon />
+        <TimesIcon />
+        <TimesIcon />
+        <TimesIcon />
+        <TimesIcon />
+      </IconGroup>,
+    );
+    expect(container.querySelectorAll('svg').length).toBe(6);
+    expect(container.querySelector('svg')).toHaveAttribute('width', '12px');
   });
 });
