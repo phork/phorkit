d6d9e92a618c531c6639e6f30af117fc31bf380d
Elenor <elenor@elenor.net>
2021-10-12 15:52:49 -0700

    :sparkles: feat: added Navigation stories

---
+++ src/compositions/Navigation/stories/helpers/items.ts
@@ -0,0 +1,6 @@
+export const items = [
+  { id: 'first', label: 'First' },
+  { id: 'second', label: 'Second' },
+  { id: 'third', label: 'Third', disabled: true },
+  { id: 'fourth', label: 'Fourth' },
+];
