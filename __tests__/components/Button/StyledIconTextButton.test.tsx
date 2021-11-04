382263d0ad6d64d56bff84a933b4903db7452bef
Elenor <elenor@elenor.net>
2021-11-04 14:20:45 -0700

    :recycle: refactor(major): only allow ReactElement icon type in icon buttons

--- __tests__/components/Button/StyledIconTextButton.test.tsx
+++ __tests__/components/Button/StyledIconTextButton.test.tsx
@@ -14,7 +14,7 @@ const styles = {
 describe('<StyledIconTextButton />', () => {
   it('should render a button', () => {
     const { container, getByText } = render(
-      <StyledIconTextButton icon={TimesIcon} {...styles}>
+      <StyledIconTextButton icon={<TimesIcon scale="medium" />} {...styles}>
         Click me!
       </StyledIconTextButton>,
     );
@@ -24,7 +24,7 @@ describe('<StyledIconTextButton />', () => {
 
   it('should render a disabled button', () => {
     const { getByTestId } = render(
-      <StyledIconTextButton disabled data-testid="button" icon={TimesIcon} {...styles}>
+      <StyledIconTextButton disabled data-testid="button" icon={<TimesIcon scale="medium" />} {...styles}>
         Click me!
       </StyledIconTextButton>,
     );
@@ -33,7 +33,13 @@ describe('<StyledIconTextButton />', () => {
 
   it('should render a loading button', () => {
     const { getByText, getByTestId } = render(
-      <StyledIconTextButton loading data-testid="button" icon={TimesIcon} loader={<span>Loading</span>} {...styles}>
+      <StyledIconTextButton
+        loading
+        data-testid="button"
+        icon={<TimesIcon scale="medium" />}
+        loader={<span>Loading</span>}
+        {...styles}
+      >
         Click me!
       </StyledIconTextButton>,
     );
@@ -47,7 +53,7 @@ describe('<StyledIconTextButton />', () => {
         loaderReplaceIcon
         loading
         data-testid="button"
-        icon={TimesIcon}
+        icon={<TimesIcon scale="medium" />}
         loader={<span>Loading</span>}
         {...styles}
       >
@@ -61,7 +67,7 @@ describe('<StyledIconTextButton />', () => {
 
   it('should render a submit button', () => {
     const { getByTestId } = render(
-      <StyledIconTextButton data-testid="button" icon={TimesIcon} type="submit" {...styles}>
+      <StyledIconTextButton data-testid="button" icon={<TimesIcon scale="medium" />} type="submit" {...styles}>
         Click me!
       </StyledIconTextButton>,
     );
@@ -71,7 +77,7 @@ describe('<StyledIconTextButton />', () => {
   it('should be clickable', () => {
     const onClick = jest.fn();
     const { getByRole } = render(
-      <StyledIconTextButton icon={TimesIcon} onClick={onClick} {...styles}>
+      <StyledIconTextButton icon={<TimesIcon scale="medium" />} onClick={onClick} {...styles}>
         Click me!
       </StyledIconTextButton>,
     );
