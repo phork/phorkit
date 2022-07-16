39e578d2acd436390671a58369d3460b3ffe71b5
Elenor <elenor@elenor.net>
2022-07-16 09:10:27 -0700

    :rewind: fix: revert AsReactType generics

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export type MergeProps<T extends object, P extends object> = Omit<T, keyof P> & P;
 
 // see https://github.com/microsoft/TypeScript/issues/36860 for css, sx removal notes
@@ -31,7 +32,7 @@ export type AsType<T extends keyof JSX.IntrinsicElements> = {
 
 // type can be div, span, etc. or a react element
 export type AsReactType<T extends React.ElementType> = {
-  as?: T | ((props: unknown) => React.ReactElement<unknown, T>);
+  as?: T | ((props: any) => React.ReactElement<any, T>);
 };
 
 export type Mutable<T> = { -readonly [P in keyof T]: T[P] };
